f0b59199c7e64b6eb66c7f4357950a3c65410e76
ASRAYUSHSINGH <144120094+ASRAYUSHSINGH@users.noreply.github.com>
2024-01-29 17:26:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState} from "react";
+import "./App.css";
+import questions from "./questions";
+import Result from "./components/Result";
+import QuestionBox from "./components/QuestionBox";
+
+
+function App() {
+  let [color,setcolor]=useState(true)
+  let[bgColor,setbgColor]=useState('')
+  let[textColor,settextColor]=useState('')
+
+  
+  function backgroundchange(){
+    setcolor(!color)
+    let white='#FFFFFF'
+    let gray='#333333'
+    let black='#000000'
+    setbgColor(color?gray:white)
+    settextColor(color?white:black)
+
+  }
+
+  return (
+    <div style={{background: bgColor }} className="body">
+      <div className="head">
+        <h1 style={{color: textColor}}>Quizlet</h1>
+        <button onClick={backgroundchange} className="modeBtn">{color?'Light':'Dark'}</button>
+      </div>
+      <div className="questionBox">
+        <QuestionBox />
+      </div>
+      
+      
+    </div>
+
+  );
+}
+
+export default App;
